b6259afbcce414f69470018fbebcee1b29e891f8
Jasper Tenga <jaspertenga24@gmail.com>
2023-02-03 22:25:38 -0500

    Testing to see branch connection

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,28 @@
-const hre = require("hardhat");
+//const hre = require("hardhat");
 const ethers = require("ethers");
+require("dotenv").config();
+const contractExtras = require("../artifacts/contracts/Arbitrage.sol/Arbitrage.json");
 const config = require("../config.json");
+const {provider, a, b, c, d, e} = require('../helpers/initialization');
+
+let abi = JSON.parse(contractExtras.abi);
+const byteCode = JSON.parse(contractExtras.byteCode);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+const factory = new ethers.ContractFactory(abi, byteCode, wallet);
 
 async function main() {
-    const Arbitrage = await hre.ethers.getContractFactory("Arbitrage");
-    const arbitrage = await Arbitrage.deploy(
+    //const Arbitrage = await hre.ethers.getContractFactory("Arbitrage");
+
+    const contract = await factory.deploy(
         config.SUSHISWAP.V2_ROUTER_02_ADDRESS,
         config.UNISWAP.V2_ROUTER_02_ADDRESS
     );
 
-    await arbitrage.deployed();
+    await contract.deployed();
 
-    console.log(`Arbitrage contract deployed to ${arbitrage.address}`);
+    console.log(`Arbitrage contract deployed to ${contract.address}`);
+    console.log(`${contract.deployTransaction}`);
 }
 
 main().catch((error) => {
